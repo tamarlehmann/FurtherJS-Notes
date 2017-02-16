9063018ad255b6131a2a5cd441d928e37275930c
Stefan Liute <stefanliute@gmail.com>
2017-02-16 17:43:51 +0000

    Last test for challenge #9 - in progress

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -7,7 +7,6 @@ function testNoteControllerCanBeInstantiated() {
 console.log("8. A NoteController can be instantiated.");
 testNoteControllerCanBeInstantiated();
 
-
 function testAppElementContainsNotesAsHTML() {
   var doubleElement = document.createElement('div');
   var list = new NoteList();
@@ -20,7 +19,21 @@ function testAppElementContainsNotesAsHTML() {
 console.log("9. The app element contains notes as HTML string.");
 testAppElementContainsNotesAsHTML();
 
+function testClickedNoteDisplaysSingleNote(){
+  var list = new NoteList();
+  list.addNote('Hello, I love Javascript!');
+  var linkDouble = document.createElement('app');
+  var noteDouble = document.createElement('singlenote');
+  var controller = new NoteController(list, linkDouble);
+  console.log(controller.noteList)
+  controller.htmlInserter();
+  controller.urlChangeShowsNote();
+  linkDouble.querySelector('a').click();
+  assert.isTrue(noteDouble.innerHTML === "Hello, I love Javascript!");
+};
 
+console.log("10. The singlenote element contains the full text of the note.");
+testClickedNoteDisplaysSingleNote()
 
 function testEmptyListOnAppStart() {
   var doubleElement = document.createElement('div');
@@ -29,30 +42,21 @@ function testEmptyListOnAppStart() {
   var controller = new NoteController(list, doubleElement);
   assert.isTrue(controller.element.innerHTML === "Please add a note");
 }
-console.log("12. When the app loads, the HTML for an empty note list is displayed");
-testEmptyListOnAppStart();
 
+console.log("11. When the app loads, the HTML for an empty note list is displayed");
+testEmptyListOnAppStart();
 
 function testNewNoteWrittenToAppElement() {
   var doubleFormElement = document.createElement('form');
-  doubleFormElement.value = "This is my test. Hi Test"
-  console.log(doubleFormElement.value)
-
+  doubleFormElement.value = "This is my test. Hi Test";
+  var doubleElement = document.createElement('div');
+  var list = new NoteList();
+  var controller = new NoteController(list, doubleElement);
+  list.addNote(doubleFormElement.value)
+  controller.htmlInserter();
+  controller.urlChangeShowsNote();
+  assert.isTrue(controller.element.innerHTML === "<ul><li><div><a href=\"#notes/0\">This is my test. Hi </a></div></li></ul>");
 }
-console.log("13. When a submit event is triggered, the HTML for a note list with the new note is written to the app element");
-testNewNoteWrittenToAppElement();
 
-// function testClickedNoteDisplaysSingleNote(){
-//   var list = new NoteList();
-//   list.addNote('Hello, I love Javascript!');
-//   var linkDouble = document.createElement('app');
-//   var noteDouble = document.createElement('singlenote');
-//   var controller = new NoteController(list, linkDouble);
-//   controller.htmlInserter();
-//   controller.urlChangeShowsNote();
-//   linkDouble.querySelector('a').click();
-//   assert.isTrue(noteDouble.innerHTML === "Hello, I love Javascript!");
-// };
-//
-// console.log("11. The singlenote element contains the full text of the note.");
-// testClickedNoteDisplaysSingleNote()
+console.log("12. When a submit event is triggered, the HTML for a note list with the new note is written to the app element");
+testNewNoteWrittenToAppElement();
